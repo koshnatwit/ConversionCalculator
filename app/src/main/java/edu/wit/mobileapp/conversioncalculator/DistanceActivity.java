5240a3975658ab3fa5bdf8cb377e3ee705cc7263
koshnatwit <55000984+koshnatwit@users.noreply.github.com>
2022-07-22 09:43:38 -0400

    Update DistanceActivity.java

--- app/src/main/java/edu/wit/mobileapp/conversioncalculator/DistanceActivity.java
+++ app/src/main/java/edu/wit/mobileapp/conversioncalculator/DistanceActivity.java
@@ -16,7 +16,6 @@ public class DistanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distance);
-        Bundle bundle = this.getIntent().getExtras();
         
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         
