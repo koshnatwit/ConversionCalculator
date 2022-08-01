2cc574be98aab5ddda8874cd4cd10cd5b45415d5
koshn <koshn@wit.edu>
2022-08-01 19:09:55 -0400

    Skeleton logic for temp activity

--- app/src/main/java/edu/wit/mobileapp/conversioncalculator/TempActivity.java
+++ app/src/main/java/edu/wit/mobileapp/conversioncalculator/TempActivity.java
@@ -12,6 +12,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class TempActivity extends AppCompatActivity {
 
     @Override
@@ -21,14 +23,14 @@ public class TempActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Spinner spinnerDistance1 = findViewById(R.id.spinner_temp1);
-        Spinner spinnerDistance2 = findViewById(R.id.spinner_temp2);
+        Spinner spinnerTemp1 = findViewById(R.id.spinner_temp1);
+        Spinner spinnerTemp2 = findViewById(R.id.spinner_temp2);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.temp, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinnerDistance1.setAdapter(adapter);
-        spinnerDistance2.setAdapter(adapter);
+        spinnerTemp1.setAdapter(adapter);
+        spinnerTemp2.setAdapter(adapter);
 
         EditText enterField = (EditText) findViewById(R.id.editTextNumberDecimal);
         TextView resultField = (TextView) findViewById(R.id.editTextNumberDecimal2);
@@ -39,6 +41,52 @@ public class TempActivity extends AppCompatActivity {
             public void onClick(View v){
                 enterField.setText("");
                 resultField.setText("");
+            }
+        });
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.0000");
+
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double value = Double.parseDouble(enterField.getText().toString());
+                String firstUnit = spinnerTemp2.getSelectedItem().toString();
+                String secondUnit = spinnerTemp1.getSelectedItem().toString();
+
+                if (firstUnit.equals("F")) {
+                    if (secondUnit.equals("C")) {
+
+                    }
+                    else if (secondUnit.equals("K")) {
+
+                    }
+                    else {
+                        resultField.setText(Double.toString(value));
+                    }
+                }
+                else if (firstUnit.equals("C")) {
+                    if (secondUnit.equals("F")) {
+
+                    }
+                    else if (secondUnit.equals("K")) {
+
+                    }
+                    else {
+                        resultField.setText(Double.toString(value));
+                    }
+                }
+                else {
+                    if (secondUnit.equals("F")) {
+
+                    }
+                    else if (secondUnit.equals("C")) {
+
+                    }
+                    else {
+                        resultField.setText(Double.toString(value));
+                    }
+                }
             }
         });
     }
